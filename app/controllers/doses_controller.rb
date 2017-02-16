e9eb86a51e0e0b1866b9b0b6be4e17d2c9a633fc
Rui Freitas <dinnerpartysaboteur@gmail.com>
2017-02-16 15:48:57 +0000

    added dose form in cocktails#show

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,10 +1,34 @@
 class DosesController < ApplicationController
+  before_action :set_cocktail, only: [:new, :create, :destroy]
+
   def new
+    @dose = Dose.new
   end
 
   def create
+    @dose = @cocktail.doses.create(dose_params)
+    @dose = Dose.new(dose_params)
+    if @dose.save
+      redirecto_to cocktail_path(@cocktail)
+    else
+      render 'cocktails'
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def set_dose
+    @dose = Dose.find(params[:id])
+  end
+
+  def dose_params
+    param.require(:dose).permit(:description, :cocktail_id, :ingredient_id)
   end
 end
