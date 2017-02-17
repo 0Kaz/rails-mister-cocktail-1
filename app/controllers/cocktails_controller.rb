a6dbe21b95880a04d4b400c2f9c385e613becbb0
Rui Freitas <dinnerpartysaboteur@gmail.com>
2017-02-17 14:12:25 +0000

    basic cocktail delete

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,5 +1,5 @@
 class CocktailsController < ApplicationController
-  before_action :set_cocktail, only: [:show]
+  before_action :set_cocktail, only: [:show, :edit, :update, :destroy]
 
   def index
     @cocktails = Cocktail.all
@@ -13,6 +13,9 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.new
   end
 
+  def edit
+  end
+
   def create
     @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
@@ -20,6 +23,14 @@ class CocktailsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def update
+  end
+
+  def destroy
+    @cocktail.destroy
+    redirect_to cocktails_path
   end
 
   private
