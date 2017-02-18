eb6c4b100d742814c8671e2ae9d0bbbc073ff13f
Rui Freitas <dinnerpartysaboteur@gmail.com>
2017-02-18 21:31:46 +0000

    corrected error making app crash

--- app/controllers/ingredients_controller.rb
+++
@@ -1,6 +0,0 @@
-class IngredientsController < ApplicationController
-  def index
-    @ingredients = Ingredient.order(:name).where("name ILIKE ?", "%#{params[term]}%")
-    render json: @ingredients.map(&:name) }
-  end
-end
