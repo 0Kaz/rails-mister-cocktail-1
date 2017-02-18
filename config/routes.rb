d0efbf11680f9d561e7f736e1b04e7b85659688c
Rui Freitas <dinnerpartysaboteur@gmail.com>
2017-02-18 17:01:16 +0000

    basic fb login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'cocktails#index'
 
-  devise_for :users, skip: :registrations
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
     resource :registration,
       only: [:new, :create, :edit, :update],
