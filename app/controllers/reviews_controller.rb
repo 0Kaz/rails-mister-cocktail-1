5d137ebf708e9e7b99e9fbc3439c7e59ce71a4fc
Rui Freitas <dinnerpartysaboteur@gmail.com>
2017-02-19 13:20:26 +0000

    new review append with ajax

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,9 +2,18 @@ class ReviewsController < ApplicationController
   def create
     @review = current_user.reviews.new(review_params)
     @review.cocktail = Cocktail.find(params[:cocktail_id])
+    @cocktail = @review.cocktail
 
     if @review.save
-      redirect_to cocktail_path(id: params[:cocktail_id])
+      respond_to do |format|
+        format.html { redirect_to cocktail_path(@cocktail) }
+        format.js
+      end
+    else
+      respond_to do |format|
+        format.html { render 'cocktails/show' }
+        format.js
+      end
     end
   end
 
